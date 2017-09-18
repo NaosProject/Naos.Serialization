58c960666f736ecc2ab003b72d6c1c6ddefeb458
Lawson Caudill <lawson@caudillclan.us>
2017-09-18 18:43:03 -0300

    Added a test for configuration manager.

--- Naos.Serialization.Test/TestBsonConfigurationManager.cs
+++ Naos.Serialization.Test/TestBsonConfigurationManager.cs
@@ -6,7 +6,53 @@
 
 namespace Naos.Serialization.Test
 {
-    public class TestBsonConfigurationManager
+    using System;
+
+    using FluentAssertions;
+
+    using Naos.Serialization.Bson;
+
+    using Xunit;
+
+    public static class TestBsonConfigurationManager
     {
+        [Fact]
+        public static void ConfigureWorksWithType()
+        {
+            BsonConfigurationManager.Configure(typeof(TestConfigure));
+            BsonConfigurationManager.Configure(typeof(TestConfigure));
+            BsonConfigurationManager.Configure<TestConfigure>();
+
+            TestConfigure.Configured.Should().BeTrue();
+        }
+
+        [Fact]
+        public static void ConfigureWorksWithGeneric()
+        {
+            BsonConfigurationManager.Configure<TestConfigure>();
+            BsonConfigurationManager.Configure<TestConfigure>();
+            BsonConfigurationManager.Configure(typeof(TestConfigure));
+
+            TestConfigure.Configured.Should().BeTrue();
+        }
+    }
+
+    public class TestConfigure : BsonConfigurationBase
+    {
+        /// <summary>
+        /// Gets a value indicating whether or not it has been configured.
+        /// </summary>
+        public static bool Configured { get; private set; }
+
+        /// <inheritdoc cref="BsonConfigurationBase" />
+        protected override void CustomConfiguration()
+        {
+            if (Configured)
+            {
+                throw new NotSupportedException("Configuration is not reentrant and should not have been called a second time.");
+            }
+
+            Configured = true;
+        }
     }
 }
