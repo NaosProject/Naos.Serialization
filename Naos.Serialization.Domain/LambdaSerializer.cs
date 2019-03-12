e5f66f10e9a2fd076f73c6d793ad7435263e7d51
Lawson Caudill <lawson@caudillclan.us>
2019-03-11 23:21:13 -0300

    Added a LambdaSerializer that can take in the Func<>'s to satisify the contract.

---
+++ Naos.Serialization.Domain/LambdaSerializer.cs
@@ -0,0 +1,92 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LambdaSerializer.cs" company="Naos">
+//    Copyright (c) Naos 2017. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Serialization.Domain
+{
+    using System;
+    using OBeautifulCode.Validation.Recipes;
+
+    /// <summary>
+    /// Serializer that is backed by <see cref="Func{T1,T2,T3,T4,T5,T6,T7,T8,T9,TResult}" />.
+    /// </summary>
+    public class LambdaSerializer : ISerializeAndDeserialize
+    {
+        private readonly Func<object, string> serializeString;
+        private readonly Func<string, Type, object> deserializeString;
+        private readonly Func<object, byte[]> serializeBytes;
+        private readonly Func<byte[], Type, object> deserializeBytes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaSerializer"/> class.
+        /// </summary>
+        /// <param name="serializeString">Serialize object to string.</param>
+        /// <param name="deserializeString">Deserialize object from string.</param>
+        /// <param name="serializeBytes">Serialize object to bytes.</param>
+        /// <param name="deserializeBytes">Deserialize object from bytes.</param>
+        public LambdaSerializer(
+            Func<object, string> serializeString,
+            Func<string, Type, object> deserializeString,
+            Func<object, byte[]> serializeBytes,
+            Func<byte[], Type, object> deserializeBytes)
+        {
+            new { serializeString }.Must().NotBeNull();
+            new { deserializeString }.Must().NotBeNull();
+            new { serializeBytes }.Must().NotBeNull();
+            new { deserializeBytes }.Must().NotBeNull();
+
+            this.serializeString = serializeString;
+            this.deserializeString = deserializeString;
+            this.serializeBytes = serializeBytes;
+            this.deserializeBytes = deserializeBytes;
+        }
+
+        /// <inheritdoc />
+        public SerializationKind SerializationKind => SerializationKind.Default;
+
+        /// <inheritdoc />
+        public Type ConfigurationType => null;
+
+        /// <inheritdoc />
+        public byte[] SerializeToBytes(object objectToSerialize)
+        {
+            return this.serializeBytes(objectToSerialize);
+        }
+
+        /// <inheritdoc />
+        public string SerializeToString(object objectToSerialize)
+        {
+            return this.serializeString(objectToSerialize);
+        }
+
+        /// <inheritdoc />
+        public T Deserialize<T>(string serializedString)
+        {
+            return (T)this.Deserialize(serializedString, typeof(T));
+        }
+
+        /// <inheritdoc />
+        public object Deserialize(string serializedString, Type type)
+        {
+            new { type }.Must().NotBeNull();
+
+            return this.deserializeString(serializedString, type);
+        }
+
+        /// <inheritdoc />
+        public T Deserialize<T>(byte[] serializedBytes)
+        {
+            return (T)this.Deserialize(serializedBytes, typeof(T));
+        }
+
+        /// <inheritdoc />
+        public object Deserialize(byte[] serializedBytes, Type type)
+        {
+            new { type }.Must().NotBeNull();
+
+            return this.deserializeBytes(serializedBytes, type);
+        }
+    }
+}
---
+++ Naos.Serialization.Test/LambdaSerializerTest.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LambdaSerializerTest.cs" company="Naos">
+//    Copyright (c) Naos 2017. All Rights Reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Serialization.Test
+{
+    using System;
+    using FakeItEasy;
+
+    using FluentAssertions;
+
+    using Naos.Serialization.Domain;
+    using Naos.Serialization.Json;
+    using Xunit;
+
+    using static System.FormattableString;
+
+    public static class LambdaSerializerTest
+    {
+        [Fact]
+        public static void LambdaSerializer__Passes_through()
+        {
+            // Arrange
+            var property1 = A.Dummy<string>();
+            var property2 = A.Dummy<string>();
+            var property3 = A.Dummy<string>();
+
+            var expected = "{"
+                           + Environment.NewLine
+                           + Invariant($"  \"property1\": \"{property1}\",") + Environment.NewLine
+                           + Invariant($"  \"property2\": \"{property2}\",") + Environment.NewLine
+                           + Invariant($"  \"property3\": \"{property3}\",") + Environment.NewLine
+                           + "  \"property4\": null" + Environment.NewLine
+                           + "}";
+
+            var test = new TestObjectForLambda { Property1 = property1, Property2 = property2, Property3 = property3, };
+            var backingSerializer = new NaosJsonSerializer();
+            var serializer = new LambdaSerializer(
+                backingSerializer.SerializeToString,
+                backingSerializer.Deserialize,
+                backingSerializer.SerializeToBytes,
+                backingSerializer.Deserialize);
+
+            // Act
+            var stringOut = serializer.SerializeToString(test);
+            var stringRoundTrip = serializer.Deserialize(stringOut, test.GetType());
+            var bytesOut = serializer.SerializeToBytes(stringRoundTrip);
+            var bytesRoundTrip = serializer.Deserialize(bytesOut, stringRoundTrip.GetType());
+            var actual = backingSerializer.SerializeToString(bytesRoundTrip);
+
+            // Assert
+            actual.Should().Be(expected);
+            stringOut.Should().Be(expected);
+        }
+    }
+
+    public class TestObjectForLambda
+    {
+        public string Property1 { get; set; }
+
+        public string Property2 { get; set; }
+
+        public string Property3 { get; set; }
+
+        public string Property4 { get; set; }
+    }
+}
